8e6764c8768d57144a608dd565996e99acb4fe64
Jackson <jacksonsteely@yahoo.com>
2024-06-10 10:14:47 -0400

    Final-E-Portfolio

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ function moveBackground(event) {
   for (let i = 0; i < shapes.length; ++i) {
     const isOdd = i % 2 !== 0;
     const boolInt = isOdd ? -1 : 1;
-    // Added rotate after tutorial
-    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px) rotate(${x * boolInt * 10}deg)`;
   }
 }
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
   if (contrastToggle) {
-    document.body.classList += " dark-theme"
-  }
-  else {
-    document.body.classList.remove("dark-theme")
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
   }
 }
 
